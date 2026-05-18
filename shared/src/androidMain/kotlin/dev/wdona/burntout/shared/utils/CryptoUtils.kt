241dfa2211a91e1a7adfa8d7e9a214457f36f1f7
wDona <donanferyt@gmail.com>
2026-05-18 12:23:12 +0200

    comment

--- shared/src/androidMain/kotlin/dev/wdona/burntout/shared/utils/CryptoUtils.kt
+++ shared/src/androidMain/kotlin/dev/wdona/burntout/shared/utils/CryptoUtils.kt
@@ -2,6 +2,7 @@ package dev.wdona.burntout.shared.utils
 
 import java.security.MessageDigest
 
+// Implementado con actual para poder usarlo en iOS en el futuro
 actual fun hashPasswordForTransport(password: String): String {
     val digest = MessageDigest.getInstance("SHA-256")
     val bytes = digest.digest(password.toByteArray(Charsets.UTF_8))
--- shared/src/commonMain/kotlin/dev/wdona/burntout/shared/utils/CryptoUtils.kt
+++ shared/src/commonMain/kotlin/dev/wdona/burntout/shared/utils/CryptoUtils.kt
@@ -1,3 +1,4 @@
 package dev.wdona.burntout.shared.utils
 
+// Implementado con actual para poder usarlo en iOS en el futuro
 expect fun hashPasswordForTransport(password: String): String
